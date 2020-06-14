1e3f0c941823c044f93ade5799f52d50dd5ea3c0
Parthe Omkar <60726851+PartheOmkar@users.noreply.github.com>
2020-06-14 09:04:50 +0530

    Add files via upload

---
+++ CPPClassTempl.cpp
@@ -0,0 +1,63 @@
+//C++ Class Templates in C++ - Hacker Rank Solution
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+/*Write the class AddElements here*/
+/* C++ Class Templates in C++ - Hacker Rank Solution START */
+template <class T> class AddElements {
+    public:
+        T element;
+        AddElements(T i) {element = i;}
+        T add(T i) {return element+i;}
+    private:
+        
+};
+template <> class AddElements <string> {
+    public:
+        string element;
+        AddElements(string i) {element = i;}
+        string concatenate(string element2) {return element+element2;}
+    private:
+        
+};
+
+/* C++ Class Templates in C++ - Hacker Rank Solution END */
+
+int main () 
+{
+  int n,i;
+  cin >> n;
+  for(i=0;i<n;i++) 
+  {
+    string type;
+    cin >> type;
+    if(type=="float") 
+    {
+        double element1,element2;
+        cin >> element1 >> element2;
+        AddElements<double> myfloat (element1);
+        cout << myfloat.add(element2) << endl;
+    }
+    else if(type == "int") 
+    {
+        int element1, element2;
+        cin >> element1 >> element2;
+        AddElements<int> myint (element1);
+        cout << myint.add(element2) << endl;
+    }
+    else if(type == "string") 
+    {
+        string element1, element2;
+        cin >> element1 >> element2;
+        AddElements<string> mystring (element1);
+        cout << mystring.concatenate(element2) << endl;
+    }
+  }
+  return 0;
+}
+
